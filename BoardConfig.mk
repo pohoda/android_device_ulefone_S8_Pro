830211c764145fa6a5584c83536a5bcb2f3375be
pohoda <lubomir.bertovic@gmail.com>
2019-12-26 23:45:01 +0100

    skusam prejst na omni-twrp-9.0-minimal

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -40,7 +40,6 @@ BOARD_GLOBAL_CPPFLAGS += -mfpu=neon -mfloat-abi=softfp
 TARGET_PREBUILT_KERNEL := $(LOCAL_PATH)/kernel
 
 # Partitions & Kernel
-
 BOARD_FLASH_BLOCK_SIZE :=	131072
 BOARD_BOOTIMAGE_PARTITION_SIZE :=	16777216
 BOARD_RECOVERYIMAGE_PARTITION_SIZE :=	16777216
@@ -83,21 +82,24 @@ PRODUCT_COPY_FILES += $(LOCAL_PATH)/fstab.mt6735:root/fstab.mt6735
 
 # TWRP recovery
 RECOVERY_VARIANT                          := twrp
-TW_DEFAULT_LANGUAGE						  := sk
+TW_DEFAULT_LANGUAGE						  := sk-SK
 TW_EXTRA_LANGUAGES						  := true
 PRODUCT_COPY_FILES += $(LOCAL_PATH)/twrp.fstab:recovery/root/etc/twrp.fstab
 HAVE_LIBSELINUX                           := true
+#TWHAVE_SELINUX                            := true
 TW_INCLUDE_CRYPTO                         := true
 #TW_NO_USB_STORAGE                         := false
 RECOVERY_GRAPHICS_USE_LINELENGTH          := true
-TW_USE_TOOLBOX                            := false
+#TW_USE_TOOLBOX                            := false
 RECOVERY_SDCARD_ON_DATA                   := true
 TW_THEME                                  := portrait_hdpi
 TW_DEFAULT_EXTERNAL_STORAGE               := true
 TW_BRIGHTNESS_PATH := /sys/class/leds/lcd-backlight/brightness
 TW_SECONDARY_BRIGHTNESS_PATH := /sys/devices/platform/leds-mt65xx/leds/lcd-backlight/brightness
 TW_CUSTOM_CPU_TEMP_PATH := /sys/devices/virtual/thermal/thermal_zone1/temp
-TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/platform/mt_usb/musb-hdrc.0/gadget/lun%d/file
+
+TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/devices/platform/mt_usb/musb-hdrc.0/gadget/lun%d/file"
+
 BOARD_HAS_NO_SELECT_BUTTON := true
 BOARD_RECOVERY_SWIPE := true
 BOARD_SUPPRESS_EMMC_WIPE := true
@@ -110,7 +112,7 @@ TW_NO_REBOOT_BOOTLOADER := false
 TW_HAS_DOWNLOAD_MODE := true
 
 # Default Brightness (TW_DEFAULT_BRIGHTNESS=255-->100%; TW_DEFAULT_BRIGHTNESS=218-->85%)
-TW_DEFAULT_BRIGHTNESS := 225
+TW_DEFAULT_BRIGHTNESS := 210
 TW_MAX_BRIGHTNESS := 255
 
 # MTK Hardware
@@ -162,11 +164,17 @@ TW_MILITARY_TIME                              := 1
 
 # For Screen_timeout_secs
 TW_NO_SCREEN_TIMEOUT					  := true
-# TW_SCREEN_TIMEOUT_SECS					  := "100"
+# TW_SCREEN_TIMEOUT_SECS					  := 100
 WITH_SU                                   := true
 TWRP_INCLUDE_LOGCAT                       := true
 TW_INCLUDE_FB2PN                          := true
-BOARD_SUPPRESS_SECURE_ERASE               := true
-BOARD_BML_BOOT 							  := "dev/block/bml8"
-BOARD_BML_RECOVERY 						  := "dev/block/bml9"
-BOARD_RECOVERY_DEFINES 					  := BOARD_BML_BOOT BOARD_BML_RECOVERY
+#BOARD_SUPPRESS_SECURE_ERASE               := true
+#BOARD_BML_BOOT 							  := dev/block/bml8
+#BOARD_BML_RECOVERY 						  := dev/block/bml9
+#BOARD_RECOVERY_DEFINES 					  := BOARD_BML_BOOT BOARD_BML_RECOVERY
+BOARD_VNDK_RUNTIME_DISABLE                := true
+#TW_NEVER_UNMOUNT_SYSTEM                   := true
+#TW_OEM_BUILD                              := true  ### kvoli tomuto error hned na zaciatku
+#TW_FORCE_CPUINFO_FOR_DEVICE_ID            := true
+#TARGET_RECOVERY_DEVICE_MODULES := true
+
